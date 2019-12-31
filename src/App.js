6cbf6ff6f95d955d263342b695b330f5187f28e1
moisesabraaoso <moisesdirt_bmx@hotmail.com>
2019-12-31 12:29:58 -0300

    versao provisoria

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="banner-text">
+          <h1>Moisés Abraão</h1>
+
+          <hr/>
+
+          <div className="social-links">
+              <a href="http://www.twitter.com/moisesabraao" rel="noopener noreferrer" _target="blank">
+                  <i class="fa fa-twitter" aria-hidden="true"></i>
+              </a>
+              <a href="http://www.github.com/moisesabraao" _target="blank">
+                  <i class="fa fa-github" aria-hidden="true"></i>
+              </a>
+              <a href="http://www.instagram.com/moisesabraao" _target="blank">
+                  <i class="fa fa-instagram" aria-hidden="true"></i>
+              </a>
+              
+          </div>
+
+          <p>Site provisório.</p>
+        </div>
       </header>
     </div>
   );
