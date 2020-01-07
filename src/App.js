f70d0a002557874eb9e52a3170852ca4072d3217
moisesabraaoso <moisesdirt_bmx@hotmail.com>
2020-01-06 22:30:30 -0300

    inclusao do telegram

--- src/App.js
+++ src/App.js
@@ -20,15 +20,26 @@ function App() {
               <a href="http://www.github.com/moisesabraao" _target="blank">
                   <i class="fa fa-github" aria-hidden="true"></i>
               </a>
-              <a href="http://www.instagram.com/moisesabraao" _target="blank">
+              {/* <a href="http://www.instagram.com/moisesabraao" _target="blank">
                   <i class="fa fa-instagram" aria-hidden="true"></i>
+              </a> */}
+              <a href="https://t.me/moisesabraao" _target="blank">
+                  <i class="fa fa-telegram" aria-hidden="true"></i>
               </a>
               
           </div>
 
-          <p>Site provisório.</p>
+          {/* <p>Site provisório.</p> */}
         </div>
       </header>
+      {/* <content className="App-content">
+        <h1>About me</h1>
+        <hr/>
+        <p>
+          Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.
+        </p>
+
+      </content> */}
     </div>
   );
 }
